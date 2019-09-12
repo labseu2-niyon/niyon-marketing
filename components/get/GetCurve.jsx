bd3f7560a4cfd06d9543ce90be993b85f5bdb15e
mattshardman <mattshardman@gmail.com>
2019-09-12 17:41:14 +0200

    create curve

---
+++ components/get/GetCurve.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GetCurve() {
+  return (
+    <svg width={500} height={350}>
+      <path d="M 0 0 Q 45 120 180 150 Q 300 170 360 350 H 500 V 0" fill="#66c23e" />
+    </svg>
+  );
+}
+
+export default GetCurve;
