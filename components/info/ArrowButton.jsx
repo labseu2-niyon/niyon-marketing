7dbd64cb9664816f4fc0dc96caa6c73f4980c2de
mattshardman <mattshardman@gmail.com>
2019-09-12 17:41:53 +0200

    create arrow button

---
+++ components/info/ArrowButton.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+function ArrowButton() {
+  const [hovered, setHovered] = useState(false);
+  return (
+    <a href="https://niyonapp.now.sh/auth/signup">
+      <button
+        type="button"
+        onMouseOver={() => setHovered(true)}
+        onFocus={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        onBlur={() => setHovered(false)}
+      >
+        <i className="material-icons">arrow_forwards</i>Sign Up Now
+      </button>
+      <style jsx>{`
+            button {
+                position: relative;
+                margin-top: 32px;
+                padding: 0 40px;
+                display: flex;
+                color: #1759e8;
+                justify-content: center;
+                align-items: center;
+                background: none;
+                border: none;
+                font-weight: 700;
+                font-size: 1.2rem;
+                cursor: pointer;
+            }
+
+            .material-icons {
+                position: absolute;
+                left: ${hovered ? '10px' : '0px'};
+                transition: left 400ms;
+            }
+
+            a {
+              text-decoration: none;
+              outline: none;
+            }
+        `}
+      </style>
+    </a>
+  );
+}
+
+export default ArrowButton;
